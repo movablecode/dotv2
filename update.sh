cc8d233b9bdffe719f0cd7adb51d9bdd2e316ed2
movablecode <sangmin.lna@gmail.com>
2019-04-26 07:54:23 +0900

    update

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env zsh
 
-echo 'HAHAHA'
 rsync --exclude ".git/" \
 	--exclude ".DS_Store" \
 	--exclude ".osx" \
 	--exclude "install.sh" \
+	--exclude "update.sh" \
 	--exclude "README.md" \
 	--exclude "CHANGELOG.md" \
 	-avh --no-perms . ~;
 source ~/.zshrc;
-echo 'HEHEHE'
+echo 'dot files updated'
 
